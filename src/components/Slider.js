e5571b98e013b24fbba6fc048c93f6539c86bdec
Casey James <casey@khaosophy.com>
2021-02-19 11:21:59 -0500

    added maxSlide functionality

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,15 +3,19 @@ import Card from './Card';
 import '../styles/slider.css';
 
 export default function Slider(props) {
+  // If maxSlides has been passed in as a prop, render only a subset of the slides
+  const slides = (props.maxSlides) ? props.slides.slice(0, props.maxSlides) : props.slides;
+
   return (
     <div className="slider">
-      {/* todo: max slides */}
-      {props.slides.map(slide => (
+      {slides.map(slide => (
         <Card
           key={slide.id}
           title={slide.title}
           subtitle={slide.subtitle}
           text={slide.text}
+          // the image URL in the supplied data was not working for testing,
+          // so I replaced it with a placeholder link that does
           image="https://picsum.photos/300/150/"
         />
       ))}
